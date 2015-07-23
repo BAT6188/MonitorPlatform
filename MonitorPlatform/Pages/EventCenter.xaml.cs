7f0af237f19e3e084151fed462d0a05d3a3236a1
abelzhu <abel_zhu@msn.com>
2015-07-23 19:07:44 +0800

    update by Abel. 0723 1.0717里第1条把第三班时间去掉吧 2.0717里第2条加两个条件筛选
    Signed-off-by: abelzhu <abel_zhu@msn.com>

--- MonitorPlatform/Pages/EventCenter.xaml.cs
+++ MonitorPlatform/Pages/EventCenter.xaml.cs
@@ -29,7 +29,7 @@ namespace MonitorPlatform.Pages
 
         public void SetDataSource()
         {
-            if (this.lines == null || this.datePicker1 ==null || this.status ==null)
+            if (this.lines == null || this.datePicker1 ==null || this.status ==null || this.type == null || this.grade == null)
             {
                 return;
             }
@@ -47,11 +47,11 @@ namespace MonitorPlatform.Pages
             //Status：0-已处理,1-未处里,2-忽略
             if (selectedText == "1号线" || selectedText == "全部" || selectedText =="")
             {
-                data1 = FilterByTime(FilterByStatus(MonitorDataModel.Instance().SubWayLines[0].EventDatas));
+                data1 = FilterByTime(FilterByStatus(FilterByType(FilterByGrade(MonitorDataModel.Instance().SubWayLines[0].EventDatas))));
             }
             if (selectedText == "2号线" || selectedText == "全部" || selectedText == "")
             {
-                data2 = FilterByTime(FilterByStatus(MonitorDataModel.Instance().SubWayLines[1].EventDatas));
+                data2 = FilterByTime(FilterByStatus(FilterByType(FilterByGrade(MonitorDataModel.Instance().SubWayLines[1].EventDatas))));
             }
             source.Clear();
             if (data1 != null)
@@ -112,6 +112,47 @@ namespace MonitorPlatform.Pages
             return data;
         }
 
+        public IEnumerable<EventData> FilterByType(IEnumerable<EventData> data)
+        {
+            string selectedText = "";
+            ComboBoxItem cbi = (ComboBoxItem)type.SelectedItem;
+            if (cbi != null)
+                selectedText = cbi.Content.ToString();
+            if (selectedText == "轨道公交接驳事件")
+            {
+                return data.Where(x => x.AType == "轨道公交接驳事件");
+            }
+            else if (selectedText == "其他")
+            {
+                return data.Where(x => x.AType != "轨道公交接驳事件");
+            }
+            return data;
+        }
+
+        public IEnumerable<EventData> FilterByGrade(IEnumerable<EventData> data)
+        {
+            string selectedText = "";
+            ComboBoxItem cbi = (ComboBoxItem)grade.SelectedItem;
+            if (cbi != null)
+                selectedText = cbi.Content.ToString();
+            if (selectedText == "一级事件")
+            {
+                return data.Where(x => x.ALevel == "一级事件");
+            }
+            else if (selectedText == "二级事件")
+            {
+                return data.Where(x => x.ALevel == "二级事件");
+            }
+            else if (selectedText == "三级事件")
+            {
+                return data.Where(x => x.ALevel == "三级事件");
+            }
+            else if (selectedText == "四级事件")
+            {
+                return data.Where(x => x.ALevel == "四级事件");
+            }
+            return data;
+        }
 
         private void datePicker1_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
         {
